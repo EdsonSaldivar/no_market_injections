20bd87b16ee24de3d80b7bedb427122745a2cffb
Edson Saldivar <137822795+EdsonSaldivar@users.noreply.github.com>
2024-08-18 16:55:41 -0600

    Update sign.js

--- router/sign.js
+++ router/sign.js
@@ -36,32 +36,3 @@ router.post('/register', async (req, res, next) => {
 });
 
 module.exports = router;
-
-/*
-const express = require('express');
-const path = require('path');
-const { getDb } = require('../utils/db');
-const router = express.Router();
-
-router.use('/sign-in', (req, res, next) => {
-    console.log("Ruta de registrar un usuario sin iniciar sesión")
-    res.sendFile(path.join(__dirname, '../views', 'sign.html'));
-});
-
-router.post('/register', async (req, res, next) => {
-    const db = getDb();
-    try {
-        await db.collection('users').insertOne({
-            username: req.body.username,
-            email: req.body.email,
-            password: req.body.password
-        });
-        res.redirect('/');
-    } catch (e) {
-        console.log(e);
-        res.status(500).send('Error registering user');
-    }
-});
-
-module.exports = router;
-*/
