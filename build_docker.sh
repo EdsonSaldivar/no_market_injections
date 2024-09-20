c16c889cf58e2794df4f4bad21ce9c8896f72cf7
Edson Saldivar <137822795+EdsonSaldivar@users.noreply.github.com>
2024-09-19 19:43:13 -0600

    Update build_docker.sh

--- build_docker.sh
+++ build_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker rm -f no_market_injections
+sudo docker rm -f no_market_injections
 
-docker build --tag=no_market_injections . && \
+sudo docker build --tag=no_market_injections . && \
 
-docker run --name=no_market_injections --rm -p 3000:3000 -it no_market_injections
+sudo docker run --name=no_market_injections --rm -p 3000:3000 -it no_market_injections
