133953435110ad250bf1f8209b781fb941c505e9
Edson Saldivar <137822795+EdsonSaldivar@users.noreply.github.com>
2024-09-19 19:41:10 -0600

    Update build_docker.sh

--- build_docker.sh
+++ build_docker.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Elimina cualquier contenedor existente con el mismo nombre
 docker rm -f no_market_injections
 
-# Construye la imagen usando el Dockerfile
 docker build --tag=no_market_injections . && \
 
-# Ejecuta el contenedor
 docker run --name=no_market_injections --rm -p 3000:3000 -it no_market_injections
