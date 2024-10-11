bcd9bccc0dc2f6034a3d1634de01deaca63a8606
Edson Saldivar <137822795+EdsonSaldivar@users.noreply.github.com>
2024-10-11 11:08:45 -0600

    Update build_docker.sh

--- build_docker.sh
+++ build_docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Detener y eliminar los contenedores si ya existen
-sudo docker-compose down
+sudo docker compose down
 
 # Construir y levantar los servicios (MongoDB y la app) definidos en docker-compose.yml
-sudo docker-compose up --build
+sudo docker compose up --build
 
